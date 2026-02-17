e2c6c729a8c3565076cac250bcb8f1fa8340f2f6
ROKHITHRAN <118052257+ROKHITHRAN@users.noreply.github.com>
2026-02-17 11:45:05 +0530

    Initial commit

---
+++ src/test/java/com/example/nursery_web/NurseryWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nursery_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseryWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
