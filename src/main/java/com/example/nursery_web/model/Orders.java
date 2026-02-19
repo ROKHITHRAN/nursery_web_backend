ba40f841182c8a1ea02845d2c11614fcbe2914f1
ROKHITHRAN <118052257+ROKHITHRAN@users.noreply.github.com>
2026-02-19 13:57:33 +0530

    Add requirements documentation and update Orders model with relationships and annotations

--- src/main/java/com/example/nursery_web/model/Orders.java
+++ src/main/java/com/example/nursery_web/model/Orders.java
@@ -1,22 +1,31 @@
 package com.example.nursery_web.model;
 
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@Setter
+@Getter
 public class Orders {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;    
 
     private Long totalPrice;
     private String timeStamp;
     private String paymentType;
+    private String status;
 
     @ManyToOne
     @JoinColumn(name = "userId")
@@ -25,18 +34,18 @@ public class Orders {
     // No-arg constructor (required by JPA since when It create object in order to 
     // push data taken from DB it will create like new Orders() and don't know abt our
     //parametrized constructor)
-    public Orders() {
-    }
-
+  
     // Constructor with parameters
-    public Orders(Long totalPrice, String timeStamp, String paymentType, Users user) {
-        this.totalPrice = totalPrice;
-        this.timeStamp = timeStamp;
-        this.paymentType = paymentType;
-        this.user = user;
-    }
+    // public Orders(Long totalPrice, String timeStamp, String paymentType, Users user) {
+    //     this.totalPrice = totalPrice;
+    //     this.timeStamp = timeStamp;
+    //     this.paymentType = paymentType;
+    //     this.user = user;
+    // }
+    @OneToMany(mappedBy = "order")
+    List<OrderItem> orderItems;
 
-     public Long getOrderId() {
+    public Long getOrderId() {
         return orderId;
     }
 
@@ -71,5 +80,4 @@ public class Orders {
     public void setUser(Users user) {
         this.user = user;
     }
-
 }
